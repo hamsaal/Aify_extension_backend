44d2c536c697c1f63d5a4d9bd420fb86367344d2
Muhammad Hamza <122325336+hamsaal@users.noreply.github.com>
2026-03-18 13:27:23 +0100

    Remove sensitive keys from keys.js
    Removed sensitive API keys and configuration details.

--- configurations/keys.js
+++ configurations/keys.js
@@ -1,16 +1,1 @@
-module.exports = {
-  google: {
-    clientID:
-      "385159666200-ast9fv4742340unu8h1b3du1qajlckam.apps.googleusercontent.com",
-    clientSecret: "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",
-  },
-  firebase: {
-    apiKey: "AIzaSyA4Re25HUER751uEbTn21Bzm9WqAtwmb60",
-  },
-  options: {
-    domain: "auth.textaify.com",
-    maxAge: 31 * 60 * 60 * 24 * 1000,
-    httpOnly: false,
-    secure: false,
-  },
-};
+
